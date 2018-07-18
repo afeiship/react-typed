4d4d2644dcbee5844af8e5484c82efc94bbd9e76
afeiship <1290657123@qq.com>
2018-07-18 17:15:42 +0800

    issue: update docs

--- index.js
+++ index.js
@@ -3,7 +3,5 @@ var HAS_DOM = typeof document !== 'undefined';
 var DEFAULT_TAG = 'section';
 
 module.exports = function (inTag) {
-  return HAS_DOM ? (inTag || DEFAULT_TAG) : React.Fragment;
+  return HAS_DOM ? (inTag || DEFAULT_TAG) : (inTag || React.Fragment);
 };
-
-
