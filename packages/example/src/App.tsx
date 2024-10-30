c5aee5edcb2e38065fb67d48bc24387d065624d6
afeiship <1290657123@qq.com>
2024-10-30 10:22:18 +0800

    feat: upt example

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,7 +1,14 @@
 import ReactTyped from '@jswork/react-typed/src';
 import '@jswork/react-typed/src/style.scss';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [str, setStr] = useState<string[]>([]);
+  useEffect(() => {
+    setTimeout(() => {
+      setStr(['Hello world, this is react-typed.']);
+    }, 1000);
+  }, []);
   return (
     <div className="m-10 p-4 shadow bg-gray-100 text-gray-800 hover:shadow-md transition-all">
       <div className="badge badge-warning absolute right-0 top-0 m-4">
@@ -18,7 +25,9 @@ function App() {
         <p>此两者，同出而异名，同谓之玄，玄之又玄，众妙之门。</p>
       </ReactTyped>
       <hr />
-      <ReactTyped strings={['Hello world, this is react-typed.']} typeSpeed={100} />
+      {
+        str.length > 0 && <ReactTyped strings={str} typeSpeed={100} />
+      }
     </div>
   );
 }
